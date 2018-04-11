a5518e674a74a9ec3f5acd7105fa76c5cc30b436
Asif Mushtaq <asif@climesoft.com>
2018-04-11 19:03:29 +0500

    Dependency Fixed

--- climesoft.data/src/module-info.java
+++ climesoft.data/src/module-info.java
@@ -3,7 +3,10 @@ module climesoft.data {
     requires protobuf.java.format;
     requires java.xml;
     requires jackson.all;
+    requires java.xml.bind;
+    requires jackson.core;
+    requires com.fasterxml.jackson.dataformat.smile;
     exports climesoft.data.json;
     exports climesoft.data.xml;
-    exports climesoft.data.protobuff;
+    exports climesoft.data.protobuf;
 }
